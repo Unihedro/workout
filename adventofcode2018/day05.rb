6023fa06eba2e4e23c404fc919e3dea05ebde1a5
Unihedron <unihedron@unihedro.com>
2018-12-05 14:23:17 +0800

    Changed code + added note

--- adventofcode2018/day05.rb
+++ adventofcode2018/day05.rb
@@ -8,7 +8,7 @@ a=$<.read
 a.chars{|x|g[-1].downcase==x.downcase && g[-1] != x ? (g[-1]='') : g<<x}
 p g.strip.size
 
-a=g.strip
+a=g.strip # This line is added afterwards because all initial reductions also apply regardless of which letter to take away, so part 1 can be used as a first pass
 p a.downcase.chars.uniq.map{|x|g=' '
 a.chars.reject{|y|y.downcase==x}.each{|x|g[-1].downcase==x.downcase && g[-1] != x ? (g[-1]='') : g<<x}
 g.strip.size
